4cdfe94c50321a262404dfc30af17b3586bca91e
SUHAIL JOKHIO <suhailmj92@gmail.com>
2024-06-16 04:57:24 -0700

    1st experience

---
+++ index.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer" 
+
+const Result = await inquirer.prompt([
+    {
+        message: "Input First Number And Press Enter Key",
+        type: "number",
+        name: "FirstNumber",
+    },
+    {
+        message: "Input Second Number And Press Enter Key",
+        type: "number",
+        name: "SecondNumber",
+    },
+    {
+        message: "Select one of the operators to perform action and press Enter Key to get your Answer! ",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    }
+]);
+// conditional statement
+
+if (Result.operator === "Addition") {
+    console.log( Result.FirstNumber + Result.SecondNumber);
+
+} else if (Result.operator === "Subtraction") {
+    console.log(Result.FirstNumber - Result.SecondNumber);
+
+}else if  (Result.operator === "Multiplication") {
+    console.log( Result.FirstNumber * Result.SecondNumber);
+
+}else if (Result.operator === "Division") {
+    console.log( Result.FirstNumber / Result.SecondNumber);
+
+};
+
